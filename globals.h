dcc601aba4b74b376f7c7b545eed01933739ddd0
ajsaucier <ajsaucier@users.noreply.github.com>
2019-08-27 15:41:10 -0400

    Rename file and create more structures

---
+++ globals.h
@@ -0,0 +1,53 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+Arduboy2 arduboy;
+
+constexpr uint8_t tileSize{ 32 };
+constexpr uint8_t borderHeight{ 8 };
+constexpr uint8_t bottomBorderLimit{ HEIGHT - borderHeight };
+constexpr uint8_t playerSize{ 16 };
+
+enum class GameStatus : uint8_t {
+  Introduction,
+  PlayGame,
+  GameOver,
+};
+
+// GameStatus gameStatus {GameStatus::Introduction}; // for production
+GameStatus gameStatus {GameStatus::PlayGame}; // for testing
+
+enum class Size : uint8_t {
+  Small,
+  Medium,
+};
+
+struct Raspberry {
+  uint8_t x;
+  uint8_t y;
+  const uint8_t *image;
+};
+
+// create player
+Raspberry player = {
+  0,
+  (HEIGHT / 2) - (playerSize / 2),
+  raspberry
+};
+
+struct Matter {
+  uint8_t x;
+  uint8_t y;
+  Size size;
+  bool enabled;
+  const uint8_t *image;
+};
+
+struct Synapse {
+  uint8_t x;
+  uint8_t y;
+  bool hit;
+  const uint8_t *image;
+};
+
+#endif
