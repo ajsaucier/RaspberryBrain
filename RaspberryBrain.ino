0ffb340486b20c4c56ffed263523895f8c2e2a81
ajsaucier <ajsaucier@users.noreply.github.com>
2019-08-27 15:39:45 -0400

    Update with game state switch statement

--- RaspberryBrain.ino
+++ RaspberryBrain.ino
@@ -1,105 +1,41 @@
-#ifndef GAME_H
-#define GAME_H
-
+// See: https://mlxxxp.github.io/documents/Arduino/libraries/Arduboy2/Doxygen/html/
+#include <Arduboy2.h>
+#include "Images.h"
 #include "globals.h"
+#include "game.h"
 
-bool moveGround = true;
-int groundX = 0;
+void setup() {
+  arduboy.boot();
+  arduboy.setFrameRate(60);
 
-void pause() {
-  // pause game with B button
+  // initialize things here
+  arduboy.initRandomSeed();
+
 }
 
-void drawBackground() {
+void loop() {
+  if (!(arduboy.nextFrame()))
+    return;
+
+  arduboy.clear();
   
-  // Check if ground should be moved
-  if (moveGround) {
-    
-    // If the ground has moved one entire tile, add new tiles to the end
-    if (groundX == 32) {
-      groundX = 0;
-      
-    }
-  
-    groundX++;
-    
-    for (uint8_t x = 0; x <= WIDTH; x++) {
-      for (uint8_t y = 0; y <= HEIGHT; y++) {
-        Sprites::drawSelfMasked(x * tileSize - groundX,  y * tileSize, brainBg, 0);
-      }
-    }
-    
-    for (uint8_t x = 0; x <= WIDTH; x++) {
-      Sprites::drawSelfMasked(x * tileSize - groundX,  0, borderTop, 0);
-    }
-    
-    for (uint8_t x = 0; x <= WIDTH; x++) {
-      Sprites::drawSelfMasked(x * tileSize - groundX,  HEIGHT - borderHeight, borderBottom, 0);
-    }
+  arduboy.pollButtons();
+
+  switch (gameStatus) {
+
+    case GameStatus::Introduction:
+      introduction();
+      break;
+
+    case GameStatus::PlayGame:
+      playGame();
+      break;
+
+    case GameStatus::GameOver:
+      gameOver();
+      break;
       
   }
 
+  arduboy.display();
 }
-
-void drawPlayer() {
-  Sprites::drawSelfMasked(player.x, player.y, player.image, 0);
-}
-
-void movePlayer() {
-  
-  // move left
-  if (arduboy.pressed(LEFT_BUTTON) && player.x > 0) {
-    player.x = player.x - 3;
-  }
-  
-  // move right
-  if (arduboy.pressed(RIGHT_BUTTON) && player.x < 100) {
-    player.x = player.x + 3;
-  }
-  
-  // move up
-  if (arduboy.pressed(UP_BUTTON) && player.y > borderHeight) {
-    player.y = player.y - 3;
-  }
-  
-  // move down
-  if (arduboy.pressed(DOWN_BUTTON) && player.y < (bottomBorderLimit - playerSize)) {
-    player.y = player.y + 3;
-  }
-}
-
-void drawTest() {
-  Sprites::drawSelfMasked(50, 42, synapse, 0);
-  Sprites::drawSelfMasked(50, 8, matterSmall, 0);
-  Sprites::drawSelfMasked(60, 8, matterMedium, 0);
-  Sprites::drawSelfMasked(70, 8, matterLarge, 0);
-}
-
-/* --------------------
-Game state functions
-----------------------*/
-
-void introduction() {
-  arduboy.setCursor(0, 0);
-  arduboy.print("Intro");
-  // Show intro graphic on this screen, maybe instructions and sound on/off
-  if (arduboy.justPressed(A_BUTTON)) {
-    gameStatus = GameStatus::PlayGame;
-  }
-}
-
-void playGame() {
-  // include all gameplay functions in here
-  drawBackground();
-  drawTest();
-  drawPlayer();
-  movePlayer();
-}
-
-void gameOver() {
-  arduboy.setCursor(0, 0);
-  arduboy.print("Over");
-  // Show current score and high score on this screen
-}
-
-#endif
